1e9e6f37ebad817092f66abd7187bb21f5c83d13
Brian K. Vagnini <brian@vagnini.net>
2021-03-17 10:00:58 -0400

    Initial commit.

---
+++ variables.py
@@ -0,0 +1,34 @@
+#to declare and assign variables
+one = 1
+two = 2
+three = 3
+four = 4
+
+# to use the variable
+print(one)
+print(two)
+print(one)
+print(two)
+print(three)
+print(four)
+
+#can overwrite a variable
+print(two)
+two = 4
+print (two)
+
+Decimal = 1.1
+print (Decimal)
+
+StringVar1 = 'Hello'
+StringVar2 = "Hello"
+#StringVar3 = 'Hello" #quote types MUST match - SyntaxError: EOL while scanning string literal
+print(StringVar1)
+print(StringVar2)
+
+IntVar = 2 + 1
+print (IntVar)
+#StringVar4 = "Hello" + 1 # TypeError: can only concatenate str (not "int") to str
+StringVar5 = "Hello" + "1"
+print (StringVar5)
+
